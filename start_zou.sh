64379df56a5884cd3db81d7847242913fe4ae7f1
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-11-14 23:24:56 +0100

    Update permissions of PostgreSQL directory if needed

--- start_zou.sh
+++ start_zou.sh
@@ -2,6 +2,13 @@
 
 [ "$(ls -A /var/lib/postgresql)" ] && echo "Running with existing database in /var/lib/postgresql" || ( echo 'Populate initial db'; cd /; tar xvjf /opt/zou/postgresql.tar.bz2 )
 
+user=$(stat -c '%U' /var/lib/postgresql/9.5)
+group=$(stat -c '%G' /var/lib/postgresql/9.5)
+if [ "${user}" != "postgres" ] || [ "${group}" != "postgres" ]; then
+    echo "/var/lib/postgresql/9.5: update owner and group"
+    chown -R postgres:postgres /var/lib/postgresql/9.5
+fi
+
 # create /var/run/postgresql
 . /usr/share/postgresql-common/init.d-functions
 create_socket_directory
